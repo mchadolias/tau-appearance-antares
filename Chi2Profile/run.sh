a76fb70b86b526e3bc522467c04389247df3bc64
Michael Chadolias <mchadolias@km3net.de>
2024-06-28 12:37:57 +0200

    Initial version of run script for running on the front end

---
+++ Chi2Profile/run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# Script to run the Chi2Profile code
+#
+# Usage: ./run_MC.sh [EXPERIMENT] [TYPE] [ORDERING]
+#
+# Author: Michael Chadolias
+
+# Load modules
+source $HOME/bash_scripts/init.sh
+source $HOME/bash_scripts/swim_env.sh
+
+# Go to the directory of the script
+cd "$(dirname "$0")"
+
+# Compile the code
+make clean && make
+
+# Check number of arguments 
+if [ "$#" -ne 4 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: ./run_MC.sh [EXPERIMENT] [TYPE]"
+    echo "EXPERIMENT: STD or TAU"
+    echo "TYPE: Free or Fixed"
+    echo "ORDERING: NO or IO"
+    echo "RECONSTRUCTION: MC or NNFit or AAFit"
+    exit
+fi
+
+BINNING="./json/ANTARES/binning_ANTARES.json"
+EXPERIMENT=$1 # "STD" or "TAU"
+TYPE=$2 # "Free" or "Fixed"
+ORDERING=$3 # "NO" or "IO"
+RECONSTRUCTION=$4 # "MC" or "NNFit" or "AAFit"
+
+# Define the json files
+
+# Set the ex
+if [ $EXPERIMENT == "STD" ]; then
+    VARIABLES="./json/ANTARES/variables_ANTARES.json"
+elif [ $EXPERIMENT == "TAU" ]; then
+    VARIABLES="./json/ANTARES/variables_ANTARES_TAU.json"
+else
+    echo "EXPERIMENT type not recognized"
+    exit
+fi
+
+if [ $RECONSTRUCTION == "MC" ]; then
+    CLASSES="./json/ANTARES/classes_ANTARES_MC.json"
+elif [ $RECONSTRUCTION == "NNFit" ]; then
+    CLASSES="./json/ANTARES/classes_ANTARES_NNFit.json"
+elif [ $RECONSTRUCTION == "AAFit" ]; then
+    CLASSES="./json/ANTARES/classes_ANTARES_AAFit.json"
+else
+    echo "RECONSTRUCTION type not recognized"
+    exit
+fi
+
+
+if [ $TYPE == "Free" ]; then
+    USER="./json/User_free.json"
+    if [ $ORDERING == "NO" ]; then
+        PARAMS="./json/parameters_Data_NO_Model_NO_free.json"
+    elif [ $ORDERING == "IO" ]; then
+        PARAMS="./json/parameters_Data_NO_Model_IO_free.json"
+    else
+        echo "Ordering not recognized"
+        exit
+    fi
+elif [ $TYPE == "Fixed" ]; then
+    USER="./json/User_fixed.json"
+    if [ $ORDERING == "NO" ]; then
+        PARAMS="./json/parameters_Data_NO_Model_NO_fixed.json"
+    elif [ $ORDERING == "IO" ]; then
+        PARAMS="./json/parameters_Data_NO_Model_IO_fixed.json"
+    else
+        echo "Ordering not recognized"
+        exit
+    fi
+else
+    echo "Type not recognized"
+    exit
+fi
+
+# Run script
+./bin/MyChi2Profile  $BINNING $CLASSES $VARIABLES $PARAMS $USER
+
+echo "============ Finished ============"
