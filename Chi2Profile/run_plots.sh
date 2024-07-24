9dbe49f0b5b89147ddd80907b497ea2459aa98f0
Michael Chadolias <mchadolias@km3net.de>
2024-07-25 01:15:22 +0200

    Add all options to seperate chi2 plots

--- Chi2Profile/run_plots.sh
+++ Chi2Profile/run_plots.sh
@@ -6,18 +6,21 @@ module load python
 # Go to the directory of the script
 cd "$(dirname "$0")"
 
+SYSTEMATICS= ("systematics" "no_systematics")
 EXPERIMENT=("STD" "TAU")
 ORDERING=("NO" "IO")
-RECONSTRUCTION=("MC" "NNFit" "AAFit")
+RECONSTRUCTION=("MC" "NNFit_full" "NNFit_dir" "AAFit_ann" "AAFit_dedx")
 
 COUNTER=1
-for EXP in "${EXPERIMENT[@]}"; do
-    for ORD in "${ORDERING[@]}"; do
-        for REC in "${RECONSTRUCTION[@]}"; do
-            echo -e "\n--------------------"
-            echo -e "Run python script ${COUNTER} for $EXP $ORD $REC"
-            python3 plot_chi_square.py --probe $EXP --ordering $ORD --reco $REC
-            COUNTER=$((COUNTER+1))
+for SYS in "${SYSTEMATICS[@]}"; do
+    for EXP in "${EXPERIMENT[@]}"; do
+        for ORD in "${ORDERING[@]}"; do
+            for REC in "${RECONSTRUCTION[@]}"; do
+                echo -e "\n--------------------"
+                echo -e "Run python script ${COUNTER} for $EXP $ORD $REC"
+                python3 plot_chi_square.py --channel $EXP --ordering $ORD --reco $REC --systematic $SYS
+                COUNTER=$((COUNTER+1))
+            done
         done
     done
 done
