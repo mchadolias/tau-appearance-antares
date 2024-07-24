3f7d10f828d3cc2238c0b4f0b21ca0a8732e9a91
Michael Chadolias <mchadolias@km3net.de>
2024-07-24 17:40:05 +0200

    Remove ordering option

--- Chi2Profile/run_all_plots.sh
+++ Chi2Profile/run_all_plots.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Record the start time
+start_time=$(date +%s)
+
 # Run plotter for all possible combinations of experiment, ordering and reconstruction
 RECOS=("MC" "NNFit_full" "NNFit_dir" "AAFit_ann" "AAFit_dedx")
 CHANNELS=("STD" "TAU")
@@ -14,21 +17,26 @@ for CHANNEL in "${CHANNELS[@]}"; do
     for RECO in "${RECOS[@]}"; do
         echo -e "\n--------------------"
         echo -e "Run python script for $RECO"
-        python3 plotter.py --type reconstruction --reco $RECO --probe $CHANNEL
+        python3 plotter.py --type reconstruction --reco $RECO --channel $CHANNEL
     done
 done
 
-echo "Running for different orderings"
-echo -e "\n--------------------"
-echo -e "Run python script for $ORDERING"
-python3 plotter.py --type ordering 
 
 echo "Running for different channels"
 for RECO in "${RECOS[@]}"; do
     for CHANNEL in "${CHANNELS[@]}"; do
         echo -e "\n--------------------"
         echo -e "Run python script for $CHANNEL"
-        python3 plotter.py --type study --probe $CHANNEL --reco $RECO
+        python3 plotter.py --type study --channel $CHANNEL --reco $RECO 
     done
 done
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate the duration
+duration=$(( end_time - start_time ))
+
+# Display the duration
+echo "Script runtime: $duration seconds"
 echo "=========== DONE ==========="
