4146d4e5a33563abf6054e212807feac553f6551
Michael Chadolias <mchadolias@km3net.de>
2024-09-04 18:38:45 +0200

    Re-arrange job script

--- Chi2Profile/job.sh
+++ Chi2Profile/job.sh
@@ -20,24 +20,29 @@ source $HOME/bash_scripts/init.sh
 echo "Preparing SWIM environment"
 source $HOME/bash_scripts/swim_env.sh
 
-python3 create_json_file.py --channel $CHANNEL --order $ORDERING  --reco $RECONSTRUCTION --systematics "$SYSTEMATICS"
-python3 create_output_directories.py
+python3 create_json_file.py --channel $CHANNEL --order $ORDERING  --reco $RECONSTRUCTION --systematics "$SYSTEMATICS" --cut $CUT
+python3 create_output_directories.py --cut $CUT
 
-# Compile the code
-#echo "Compiling the code"
-#make clean && make
+sleep 5
 
 echo "Starting script:" $(basename $BASH_SOURCE)
 echo "CHANNEL: $CHANNEL"
 echo "ORDERING: $ORDERING"
 echo "RECONSTRUCTION: $RECONSTRUCTION"
 echo "SYSTEMATICS: $SYSTEMATICS"
-
+echo "CUT: $CUT"
 
 ### Define all json files
 
+if [ $SYSTEMATICS == "0" ]; then
+    SYSTEMATICS="no_systematics"
+    echo "SYSTEMATICS: $SYSTEMATICS"
+else
+    SYSTEMATICS="systematics"
+fi
+
 # Define binning json file
-BINNING="./json/ANTARES/binning_ANTARES.json"
+BINNING="./json/ANTARES/binning_ANTARES_21.json"
 
 # Define variables json file
 if [ $CHANNEL == "STD" ]; then
@@ -51,50 +56,39 @@ fi
 
 # Define classes json file
 if [ $RECONSTRUCTION == "MC" ]; then
-    CLASSES="./json/ANTARES/classes_ANTARES_MC.json"
+    CLASSES="./json/ANTARES/classes_ANTARES_${CUT}_MC.json"
 elif [ $RECONSTRUCTION == "NNFit_full" ]; then
-    CLASSES="./json/ANTARES/classes_ANTARES_NNFit_full.json"
+    CLASSES="./json/ANTARES/classes_ANTARES_${CUT}_NNFit_full.json"
 elif [ $RECONSTRUCTION == "NNFit_dir" ]; then
-    CLASSES="./json/ANTARES/classes_ANTARES_NNFit_dir.json"
+    CLASSES="./json/ANTARES/classes_ANTARES_${CUT}_NNFit_dir.json"
 elif [ $RECONSTRUCTION == "AAFit_ann" ]; then
-    CLASSES="./json/ANTARES/classes_ANTARES_AAFit_ann.json"
+    CLASSES="./json/ANTARES/classes_ANTARES_${CUT}_AAFit_ann.json"
 elif [ $RECONSTRUCTION == "AAFit_dedx" ]; then
-    CLASSES="./json/ANTARES/classes_ANTARES_AAFit_dedx.json"
+    CLASSES="./json/ANTARES/classes_ANTARES_${CUT}_AAFit_dedx.json"
 else
     echo "RECONSTRUCTION type not recognized"
     exit
 fi
 
-if [ $SYSTEMATICS == "1" ]; then
-    # Define parametes json file
-    if [ $ORDERING == "NO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_free_systematics.json"
-    elif [ $ORDERING == "IO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_free_systematics.json"
-    else
-        echo "ORDERING type not recognized"
-        exit
-    fi
 
-    # Define user json file
-    USER="./json/USER/User_${RECONSTRUCTION}_${CHANNEL}_${ORDERING}_free_systematics.json"
-else 
-    # Define parametes json file
-    if [ $ORDERING == "NO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_free_no_systematics.json"
-    elif [ $ORDERING == "IO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_free_no_systematics.json"
-    else
-        echo "ORDERING type not recognized"
-        exit
-    fi
+if [ $ORDERING == "NO" ]; then
+    PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_${SYSTEMATICS}_free.json"
+elif [ $ORDERING == "IO" ]; then
+    PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_${SYSTEMATICS}_free.json"
+else
+    echo "ORDERING type not recognized"
+    exit
+fi
+# Define user json file
+USER="./json/USER/User_${RECONSTRUCTION}_${CHANNEL}_${ORDERING}_${CUT}_${SYSTEMATICS}_free.json"
 
-    # Define user json file
-    USER="./json/USER/User_${RECONSTRUCTION}_${CHANNEL}_${ORDERING}_free_no_systematics.json"
-fi 
+## Check if the json files exist
+if [ ! -f $BINNING ] || [ ! -f $VARIABLES ] || [ ! -f $CLASSES ] || [ ! -f $PARAMS ] || [ ! -f $USER ]; then
+    echo "One or more json files do not exist"
+    exit
+fi
 
 ## Run source-code
-
 # --------------- Free ----------------
 echo "Running MyChi2Profile with TauNorm parameter free"
 echo "Parameters: $BINNING $CLASSES $VARIABLES $PARAMS $USER"
@@ -104,50 +98,25 @@ echo "Parameters: $BINNING $CLASSES $VARIABLES $PARAMS $USER"
 # --------------- Fixed ----------------
 echo "Running MyChi2Profile with TauNorm parameter fixed"
 
-if [ $SYSTEMATICS == "1" ]; then
-    # Define parametes json file
-    if [ $ORDERING == "NO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_fixed_systematics.json"
-    elif [ $ORDERING == "IO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_fixed_systematics.json"
-    else
-        echo "ORDERING type not recognized"
-        exit
-    fi
-
-    # Define user json file
-    USER="./json/USER/User_${RECONSTRUCTION}_${CHANNEL}_${ORDERING}_fixed_systematics.json"
-else 
-    # Define parametes json file
-    if [ $ORDERING == "NO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_fixed_no_systematics.json"
-    elif [ $ORDERING == "IO" ]; then
-        PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_fixed_no_systematics.json"
-    else
-        echo "ORDERING type not recognized"
-        exit
-    fi
-
-    # Define user json file
-    USER="./json/USER/User_${RECONSTRUCTION}_${CHANNEL}_${ORDERING}_fixed_no_systematics.json"
+if [ $ORDERING == "NO" ]; then
+    PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_${SYSTEMATICS}_fixed.json"
+elif [ $ORDERING == "IO" ]; then
+    PARAMS="./json/PARAMETERS/parameters_Data_NO_Model_${ORDERING}_${SYSTEMATICS}_fixed.json"
+else
+    echo "ORDERING type not recognized"
+    exit
 fi
+
+# Define user json file
+USER="./json/USER/User_${RECONSTRUCTION}_${CHANNEL}_${ORDERING}_${CUT}_${SYSTEMATICS}_fixed.json"
+
+if [ ! -f $USER ]; then
+    echo "The USER json file does not exist"
 
 ./bin/MyChi2Profile  $BINNING $CLASSES $VARIABLES $PARAMS $USER
 
 # Plotting the Chi-Square profile
 echo "Plotting the Chi-Square profile"
-
-# Define the path to the Anaconda installation's 'bin' directory2
-WORKDIR="/sps/km3net/users/mchadoli/master_thesis/tau_appearance/Chi2Profile"
-
-cd ${WORKDIR}
-
-if [ $SYSTEMATICS == "0" ]; then
-    SYSTEMATICS="no_systematics"
-    echo "SYSTEMATICS: $SYSTEMATICS"
-else
-    SYSTEMATICS="systematics"
-fi
 
 python3 plot_chi_square.py --channel $CHANNEL --ordering $ORDERING --reco $RECONSTRUCTION --systematic $SYSTEMATICS
 
