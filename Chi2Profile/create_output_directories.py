ee685290d8f1873a3209f7f414a40dd465eec6b6
Michael Chadolias <mchadolias@km3net.de>
2024-09-23 17:41:39 +0200

    Add directories for smearing

--- Chi2Profile/create_output_directories.py
+++ Chi2Profile/create_output_directories.py
@@ -11,7 +11,22 @@ def ArgumentParser():
     
 def create_directories(base_dir, cut):
     # Define the categories and possible values
-    reco_list = ['MC', 'AAFit_dedx', 'AAFit_ann', 'NNFit_full', "NNFit_dir"]
+    smeared_level_list = [
+        'km3net', 
+        '10_percent',
+        '50_percent',
+        '100_percent',
+        '200_percent',
+        '500_percent',
+        "antares"]
+    
+    reco_list = [
+        'MC',
+        'AAFit_dedx',
+        'AAFit_ann',
+        'NNFit_full',
+        "NNFit_dir"]
+    
     channel_list = ['STD', 'TAU']
     type_list = ['NO', 'IO']
     fixed_free_list = ['fixed', 'free']
@@ -24,13 +39,23 @@ def create_directories(base_dir, cut):
     # Generate all combinations using itertools.product
     for sys_option in ("systematics", "no_systematics"):
         for reco, channel, type_, ff, experiment in product(reco_list, channel_list, type_list, fixed_free_list, experiment_list):
-            dir_path = base_path / experiment / cut / sys_option / reco / channel / type_ / ff
+            
+            dir_path = base_path / experiment / cut / "unsmeared" /  sys_option / reco / channel / type_ / ff
 
             # Check if the directory already exists
             if not dir_path.exists():
                 dir_path.mkdir(parents=True, exist_ok=True)
                 print(f"Created directory: {dir_path}")
                 counter += 1
+
+        for channel, type_, ff, experiment, smeared_level in product(channel_list, type_list, fixed_free_list, experiment_list, smeared_level_list):
+            
+            dir_path = base_path / experiment / cut / "smeared" / smeared_level / sys_option / channel / type_ / ff
+
+            # Check if the directory already exists
+            if not dir_path.exists():
+                dir_path.mkdir(parents=True, exist_ok=True)
+                print(f"Created directory: {dir_path}")
     
     if counter == 0:
         print("\nAll directories already exist.")
