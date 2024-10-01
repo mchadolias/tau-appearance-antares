a09ce9b14f5f0763ba206f41d63c3833fed13435
Michael Chadolias <mchadolias@km3net.de>
2024-10-01 11:28:46 +0200

    Set global variables the lists

--- Chi2Profile/create_directories.py
+++ Chi2Profile/create_directories.py
@@ -11,59 +11,72 @@ def ArgumentParser():
     parser.add_argument("--cut", type=str, default="muon_free",
                         help="Choose the cut to be applied")
     return parser.parse_args()
+
+# Global variables
+SMEARED_LEVEL = [
+    'orca6',
+    'orca115', 
+    '10_percent',
+    '50_percent',
+    "70_percent",
+    '100_percent',
+    '200_percent',
+    '500_percent',
+    "antares"
+]
     
+RECO_LIST = [
+    'MC',
+    'AAFit_dedx',
+    'AAFit_ann',
+    'NNFit_full',
+    "NNFit_dir"
+]
     
+CHANNEL_LIST = [
+    'STD', 
+    'TAU'
+]
+
+TYPE_LIST = [
+    'NO', 
+    'IO'
+]
+FIT_LIST = [
+    'fixed', 
+    'free'
+]
+
+SYS_LIST = [
+    "no_systematics",
+    "systematics",
+]
+
 def output_directory(
     base_dir,
     cut
 ):
-    # Define the categories and possible values
-    smeared_level_list = [
-        'km3net', 
-        '10_percent',
-        '50_percent',
-        "70_percent",
-        '100_percent',
-        '200_percent',
-        '500_percent',
-        "antares"]
-    
-    reco_list = [
-        'MC',
-        'AAFit_dedx',
-        'AAFit_ann',
-        'NNFit_full',
-        "NNFit_dir"]
-    
-    channel_list = ['STD', 'TAU']
-    type_list = ['NO', 'IO']
-    fixed_free_list = ['fixed', 'free']
-    experiment_list = ['ANTARES']
     
     # Create the base directory
-    base_path = Path(os.path.join(base_dir, "output"))
+    base_path = Path(os.path.join(base_dir, "output", "ANTARES", cut))
 
     counter = 0
     # Generate all combinations using itertools.product
-    for sys_option in ("systematics", "no_systematics"):
-        for reco, channel, type_, ff, experiment in product(reco_list, channel_list, type_list, fixed_free_list, experiment_list):
-            
-            dir_path = base_path / experiment / cut / "unsmeared" /  sys_option / reco / channel / type_ / ff
+    for reco, channel, type_, ff, sys_option in product(RECO_LIST,CHANNEL_LIST, TYPE_LIST, FIT_LIST, SYS_LIST):
+        
+        dir_path = base_path / "unsmeared" /  sys_option / reco / channel / type_ / ff
+        # Check if the directory already exists
+        if not dir_path.exists():
+            dir_path.mkdir(parents=True, exist_ok=True)
+            print(f"Created directory: {dir_path}")
+            counter += 1
 
-            # Check if the directory already exists
-            if not dir_path.exists():
-                dir_path.mkdir(parents=True, exist_ok=True)
-                print(f"Created directory: {dir_path}")
-                counter += 1
-
-        for channel, type_, ff, experiment, smeared_level in product(channel_list, type_list, fixed_free_list, experiment_list, smeared_level_list):
-            
-            dir_path = base_path / experiment / cut / "smeared" / smeared_level / sys_option / channel / type_ / ff
-
-            # Check if the directory already exists
-            if not dir_path.exists():
-                dir_path.mkdir(parents=True, exist_ok=True)
-                print(f"Created directory: {dir_path}")
+    for channel, type_, ff, sys_option, smeared_level in product(CHANNEL_LIST, TYPE_LIST, FIT_LIST, SYS_LIST, SMEARED_LEVEL):
+        dir_path = base_path / "smeared" / smeared_level / sys_option / channel / type_ / ff
+        # Check if the directory already exists
+        if not dir_path.exists():
+            dir_path.mkdir(parents=True, exist_ok=True)
+            print(f"Created directory: {dir_path}")
     
     if counter == 0:
         print("\nAll directories already exist.")
@@ -78,9 +91,8 @@ def plot_directory(
 
     # Define the categories and possible values
     type = ["reconstruction", "study", "smeared"]
-    sys_option = ["systematics", "no_systematics"]
 
-    for sys_option, type in product(sys_option, type):
+    for sys_option, type in product(SYS_LIST, type):
         dir_path = base_path / sys_option / type
 
         # Check if the directory already exists
@@ -90,14 +102,16 @@ def plot_directory(
         else:
             print(f"Directory already exists: {dir_path}")
 
-if __name__ == '__main__':
-    
+def main():
     args = ArgumentParser()
     base_directory = args.base_directory
     cut = args.cut
-
+    
     # Create the directory structure
     output_directory(base_directory, cut)
 
     # Create plot directory
     plot_directory(base_directory, cut)
+
+if __name__ == "__main__":
+    main()
