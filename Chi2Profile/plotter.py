766c38205b42d27a73ec1e5543a6d2936c0cf42b
Michael Chadolias <mchadolias@km3net.de>
2024-10-03 23:20:34 +0200

    Chnages in plotter style and tidying up plot function especially the smeared option

--- Chi2Profile/plotter.py
+++ Chi2Profile/plotter.py
@@ -1,21 +1,14 @@
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as font_manager
 import numpy as np
-import uproot
 import argparse
 from itertools import product
-import os
-import scienceplots
+import sys
+import os 
+import uproot
+sys.path.append("..")
+import libraries as lib
+lib.customize_style("python")
 
-# Set the style of the plots
-plt.style.use(['science', "grid","no-latex"])   
-plt.rcParams['savefig.dpi'] = 1000
-plt.rcParams.update({
-    'axes.titlesize': 20,
-    'axes.labelsize': 15,
-    'xtick.labelsize': 12,
-    'ytick.labelsize': 12
-})
 
 text_conversion = {
     "STD": "CC",
@@ -28,15 +21,25 @@ text_conversion = {
     "AAFit_dedx": "AAFit dedx",
     "AAFit_ann": "AAFit ANN",
     "NNFit_full": "NNFit",
-    "NNFit_dir": "NNFit direction reco only",
-    "10_percent": "10 %",
-    "50_percent": "50 %",
-    "70_percent": "70 %",
-    "100_percent": "100 %",
-    "200_percent": "200 %",
-    "500_percent": "500 %",
-    "antares": "ANTARES level"
+    "NNFit_dir": "NNFit Dir Only",
+    "10_percent": "10%",
+    "50_percent": "50%",
+    "70_percent": "70%",
+    "100_percent": "100%",
+    "200_percent": "200%",
+    "500_percent": "500%",
+    "antares": "ANTARES",
+    "orca6": "ORCA 6DUs",
+    "orca115": "ORCA 115DUs",
 }
+
+RECOS_LIST = [
+    "MC",
+    "AAFit_dedx",
+    "AAFit_ann",
+    "NNFit_full",
+    "NNFit_dir",
+]
 
 def ArgumentParser():
     parser = argparse.ArgumentParser(description='Plotter for chi2 profile')
@@ -101,78 +104,61 @@ def plot_chi2(
     # Set data path
     path = os.path.join(path, "output", "ANTARES", cut)
 
-    # Define the mass ordering
-    ordering = ["NO"]
-    
-    # Define the font for legend
-    font = font_manager.FontProperties(family='sans-serif', style='normal', size=12)
-    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-
+    fig, ax = plt.subplots()
     print(f"Plotting chi-square plots for {type} type...")
     if type == "reconstruction":
-        recos = ["MC", "AAFit_dedx", "AAFit_ann", "NNFit_full", "NNFit_dir"]
-
-        for order in ordering:
             print(f"Plotting for {order}...")
-            zoomed_ax = fig.add_axes([0.6, 0.6, 0.29, 0.26])
-            for reco in recos:
+            # zoomed_ax = fig.add_axes([0.6, 0.6, 0.29, 0.26])
+            for reco in RECOS_LIST:
                 data_fixed, data_free = load_data(channel, order, os.path.join(path, "unsmeared", systematic), reco)
-                print(len(data_fixed["TauNorm"]), len(data_fixed["chi2"] - data_free["chi2"]))
-                ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"], 
-                        'o', markersize=7, label=f"{text_conversion[reco]}")
-                zoomed_ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"],
-                                 'o--', linewidth=2, markersize=5)
-            zoomed_ax.set(
-                xlim = (0, 2),
-                ylim = (0, 100),
-            )
+                ax.plot(
+                    data_fixed["TauNorm"], 
+                    data_fixed["chi2"] - data_free["chi2"], 
+                    'o', 
+                    label=f"{text_conversion[reco]}")
+            #     zoomed_ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"],
+            #                      'o--')
+            # zoomed_ax.set(
+            #     xlim = (0, 2),
+            #     ylim = (0, 100),
+            # )
             ax.set(
                 title = f"Chi2 profile for {text_conversion[channel]} channel and \
                 {text_conversion[order]}",
                 ylim = (0, 750),
-            )        
-    elif type == "study":
-        channels = ["STD", "TAU"]
-        
-        zoomed_ax = fig.add_axes([0.5, 0.5, 0.39, 0.36]) 
-        for order, channel in product(ordering, channels):
-            data_fixed, data_free = load_data(channel, order, os.path.join(path, "unsmeared", systematic), reco)
-            ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"], 
-                    'o', markersize=7, label=f"{text_conversion[order]} ({text_conversion[channel]})")
-            zoomed_ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"],
-                            'o--', linewidth=2, markersize=5)
-        ax.set(
-            title = f"Chi2 profile for {reco} reco",
-        )    
+            )          
     elif type == "smeared":
         channel = "STD"
         order = "NO"
         
         smear_path = os.path.join(path, "smeared")
-        smear_levels = os.listdir(smear_path) ;  smear_levels.remove("km3net")
+        smear_levels = os.listdir(smear_path) 
+        #smear_levels.remove("orca6") ; smear_levels.remove("orca115")
     
         fig, ax = plt.subplots(1, 1, figsize=(10, 8))
         for level in smear_levels:
             print(f"Loading smeared data for {text_conversion[level]} smearing level...")
             data_fixed, data_free = load_data( channel, order, os.path.join(smear_path, level, systematic))
             ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"],
-                    'o--', markersize=3, label=f"Smeared - {text_conversion[level]}")
+                    'o--', label=f"Smeared - {text_conversion[level]}")
             
         for reco in ["MC", "NNFit_full"]:
             data_fixed, data_free = load_data(channel, order, os.path.join(path, "unsmeared", systematic), reco)
             ax.plot(data_fixed["TauNorm"], data_fixed["chi2"] - data_free["chi2"],
-                    'o--', markersize=3, label=f"{text_conversion[reco]}")
+                    'o--', label=f"{text_conversion[reco]}")
         ax.set(
             ylim = (0, None),
             title = f"Chi-square profile for smeared events",
         )
+    else:
+        pass
     # General settings
     ax.set(
         xlabel="Tau Normalization ($N_{\\nu_{\\tau}}$)",
         ylabel="$\\Delta \\chi^2$",
         xlim=(0, 2),
     )
-    ax.legend(prop=font, frameon=False)
+    ax.legend(frameon=False)
     fig.tight_layout()
     if (type == "reconstruction"):
         fig.savefig(os.path.join(save_path, f"Chi2Profile_{type}_{channel}_{order}_{reco}.png"))
@@ -194,49 +180,25 @@ def sigma_plots(
     # Set data path
     path = os.path.join(path, "output", "ANTARES", cut_option)
 
-    # Define the mass ordering        
-    ordering = ["NO", "IO"]
-    
-    # Define the font for legend
-    font = font_manager.FontProperties(family='sans-serif', style='normal', size=10)
-    
     print(f"\nPlotting sigma plots for {type}...")
-    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+    fig, ax = plt.subplots()
     if type == "reconstruction":
-        recos = ["MC", "AAFit_dedx", "AAFit_ann", "NNFit_full", "NNFit_dir"]
 
-        for order in ordering:
-            for reco in recos:
-                data_fixed, data_free = load_data(channel, order, os.path.join(path, "unsmeared", systematic), reco)
-                ax.plot(
-                    data_fixed["TauNorm"], 
-                    square_root(data_fixed["chi2"] - data_free["chi2"]), 
-                    'o--', 
-                    linewidth=2, 
-                    markersize=4, 
-                    label=f"{text_conversion[reco]}"
-            )
-        
-    elif type == "study":
-        channels = ["STD", "TAU"]
-        
-        for order, channel in product(ordering, channels):
+        for reco in RECOS_LIST:
             data_fixed, data_free = load_data(channel, order, os.path.join(path, "unsmeared", systematic), reco)
             ax.plot(
-                data_fixed["TauNorm"],
+                data_fixed["TauNorm"], 
                 square_root(data_fixed["chi2"] - data_free["chi2"]), 
                 'o--', 
-                linewidth=2, 
-                markersize=5, 
-                label=f"{text_conversion[order]} ({text_conversion[channel]})"
+                label=f"{text_conversion[reco]}"
             )
-    
     elif type == "smeared":
         channel = "STD"
         order = "NO"
         
         smear_path = os.path.join(path, "smeared")
-        smear_levels = os.listdir(smear_path) ;  smear_levels.remove("km3net")
+        smear_levels = os.listdir(smear_path) 
+        #smear_levels.remove("orca6") ; smear_levels.remove("orca115")
 
         for level in smear_levels:
             print(f"Loading smeared data for {text_conversion[level]} smearing level...")
@@ -244,8 +206,7 @@ def sigma_plots(
             ax.plot(
                 data_fixed["TauNorm"], 
                 square_root(data_fixed["chi2"] - data_free["chi2"]),
-                'o--', 
-                markersize=3, 
+                'o--',
                 label=f"Smeared - {text_conversion[level]}"
             )
 
@@ -254,14 +215,14 @@ def sigma_plots(
             ax.plot(
                 data_fixed["TauNorm"], 
                 square_root(data_fixed["chi2"] - data_free["chi2"]),
-                'o--', 
-                markersize=3, 
+                'o--',                
                 label=f"{text_conversion[reco]}"
             )
-
+    else:
+        pass
     # General settings
     ax.axhline(y=3, linestyle='--', c = "black")
-    ax.legend(prop=font, frameon=False, loc = 1)
+    ax.legend(frameon=False)
     ax.axhline(y=5, linestyle='-.', c = "black")
     ax.set(
         xlabel="Tau Normalization ($N_{\\nu_{\\tau}}$)",
@@ -293,9 +254,6 @@ if __name__ == '__main__':
     if type ==  "reconstruction":
         if channel is None:
             raise ValueError("Please provide the channel channel")
-    elif type == "study":
-        if reco is None:
-            raise ValueError("Please provide the reconstruction algorithm")
         
     # Check if the paths exist
     if not os.path.exists(path):
